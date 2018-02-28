538c6de3381ab7ede5a2f0a31ee46f3c20424c58
Sierra Clibourne <clibo100@mail.chapman.edu>
2018-02-28 09:44:53 -0800

    i did a lot ok

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 #include <string>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stack>
+#include <algorithm>
+#include <list>
 
 using namespace std;
 
@@ -36,82 +37,202 @@ int main()
 	}
 
 	//print out board
+	cout<< '\n' << "Given board: " << '\n';
 	for(int row = 0; row < 9; ++row)
 	{
 		for(int column = 0; column < 9; ++column)
 		{
-			cout<<board[row][column]<<" ";
+			if (column != 2 && column != 5)
+			{
+				cout<<board[row][column]<<" ";
+			}
+			else
+			{
+				cout<<board[row][column]<<"  ";
+			}
 		}
-		cout<<'\n';
-	}
-
-	//find all of the rows with problems
-	stack<int> wrongRows;
-
-	int count = 1;
-	for (int row = 0; row < 9; ++row)
-	{
-		int sum = 0;
-		for (int column = 0; column < 9; ++column)
+		if (row != 2 && row != 5)
 		{
-			sum += board[row][column];
-		}
-
-		if (sum != 45)
-		{
-			wrongRows.push(row);
+			cout<<'\n';
 		}
 		else
 		{
-			int* doesExist = new int[10];
-			for (int i = 1; i < 10; ++i)
-			{
-				for (int column = 0; column < 9; ++column)
-				{
-					if (i == board[row][column])
-					{
-						doesExist[i] = true;
-					}
-				}
-			}
-
-			bool beenPushed = false;
-
-			for (int i = 0; i < 9; ++i)
-			{
-				if (doesExist[i] == false)
-				{
-					if (beenPushed == false)
-					{
-						wrongRows.push(row);
-						beenPushed = true;
-						cout<<"AAAAAAAAAAAAAAAAAAAAA"<<count<<endl;
-						++count;
-					}
-				}
-				else
-				{
-					cout<<"lets see what this does"<<count<<endl;
-					++count;
-				}
-			}
+			cout<<'\n'<<'\n';
 		}
 	}
 
-	//find all the columns with problems
-	stack<int> wrongColumns;
+	cout<<'\n';
+
+	//find all of the rows with problems
+	int* validRows = new int[9];
+
+	for (int row = 0; row < 9; ++row)
+	{
+		int temp [9];
+		for (int column = 0; column < 9; ++column)
+		{
+			temp[column] = board[row][column];
+		}
+		sort(temp, temp + 9);
+		int goal[9];
+		for (int i = 0; i < 9; ++i)
+		{
+			goal[i] = i+1;
+		}
+
+		int sum = 0;
+
+		for (int i = 0; i < 9; ++i)
+		{
+			if (goal[i] == temp[i])
+			{
+				sum++;
+			}
+		}
+
+		if (sum == 9)
+		{
+			validRows[row] = 1;
+		}
+		else
+		{
+			validRows[row] = 0;
+		}
+	}
+
+	int allRowCheck = 0;
+	for (int i = 0; i < 9; ++i)
+	{
+		if (validRows[i] == 1)
+		{
+			allRowCheck++;
+			if (allRowCheck == 9)
+			{
+				cout<<"all rows are gung ho captain"<<endl;
+			}
+		}
+		else
+		{
+			cout<<"row "<<i + 1<<" is not loving life right now"<<endl;
+		}
+	}
+
+	//find all of the columns with problems
+	int* validColumns = new int[9];
 
 	for (int column = 0; column < 9; ++column)
 	{
-		int sum = 0;
+		int temp [9];
 		for (int row = 0; row < 9; ++row)
 		{
-			sum += board[row][column];
+			temp[row] = board[row][column];
+		}
+		sort(temp, temp + 9);
+		int goal[9];
+		for (int i = 0; i < 9; ++i)
+		{
+			goal[i] = i+1;
 		}
 
-		if (sum != 45)
+		int sum = 0;
+
+		for (int i = 0; i < 9; ++i)
 		{
-			wrongColumns.push(column);
+			if (goal[i] == temp[i])
+			{
+				sum++;
+			}
+		}
+
+		if (sum == 9)
+		{
+			validColumns[column] = 1;
+		}
+		else
+		{
+			validColumns[column] = 0;
+		}
+	}
+
+	int allColumnCheck = 0;
+	for (int i = 0; i < 9; ++i)
+	{
+		if (validColumns[i] == 1)
+		{
+			allColumnCheck++;
+			if (allColumnCheck == 9)
+			{
+				cout<<"all columns are gung ho captain"<<endl;
+			}
+		}
+		else
+		{
+			cout<<"column "<<i + 1<<" is not loving life right now"<<endl;
+		}
+	}
+
+	int* validGrids = new int[9];
+	int currGrid = 0;
+
+	for (int row = 0; row < 9; row += 3)
+	{
+		for (int column = 0; column < 9; column += 3)
+		{
+			int temp [9];
+			temp[0] = board[row][column];
+			temp[1] = board[row][column+1];
+			temp[2] = board[row][column+2];
+			temp[3] = board[row+1][column];
+			temp[4] = board[row+1][column+1];
+			temp[5] = board[row+1][column+2];
+			temp[6] = board[row+2][column];
+			temp[7] = board[row+2][column+1];
+			temp[8] = board[row+2][column+2];
+
+			sort(temp, temp + 9);
+
+			int goal[9];
+			for (int i = 0; i < 9; ++i)
+			{
+				goal[i] = i+1;
+			}
+
+			int sum = 0;
+
+			for (int i = 0; i < 9; ++i)
+			{
+				if (goal[i] == temp[i])
+				{
+					sum++;
+				}
+			}
+
+			if (sum == 9)
+			{
+				validGrids[currGrid] = 1;
+			}
+			else
+			{
+				validGrids[currGrid] = 0;
+			}
+			++currGrid;
+		}
+	}
+
+	int allGridCheck = 0;
+	for (int i = 0; i < 9; ++i)
+	{
+		if (validGrids[i] == 1)
+		{
+			allGridCheck++;
+			if (allGridCheck == 9)
+			{
+				cout<<"all grids are gung ho captain"<<endl;
+			}
+		}
+		else
+		{
+			cout<<"grid "<<i + 1<<" is not loving life right now"<<endl;
 		}
 	}
 
